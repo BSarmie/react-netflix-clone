d30da08fed7ac39d343d2b902c0a64636eb434c5
Bryan Sarmiento <SarmientoB2@gmail.com>
2022-10-27 03:11:34 -0700

    Project Setup and Created Navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEjC_c0p56bI-nxP-uDPycPQEnUU76gnE",
+    authDomain: "react-netflix-clone-8c5da.firebaseapp.com",
+    projectId: "react-netflix-clone-8c5da",
+    storageBucket: "react-netflix-clone-8c5da.appspot.com",
+    messagingSenderId: "1080249022586",
+    appId: "1:1080249022586:web:c6b9333c4ccaedc74623c3"
+  };
