9226004470ed277b42908519ee0cf1e622d958f5
Bryan Sarmiento <SarmientoB2@gmail.com>
2022-10-29 03:19:51 -0700

    Updated Banner and Added TMDB API

--- src/Banner.js
+++ src/Banner.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import './Banner.css';
-import NetflixBanner from './images/netflix-banner.png';
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import NetflixBanner from "./images/netflix-banner.png";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
 
-    function truncate(string, n) {
-        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
     }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
 
   return (
-    <header 
-        className='banner' 
-        style={{
-            backgroundSize: 'cover',
-            backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
-            backgroundPosition: 'center center',
-    }}>
-        <div className="banner__contents">
-            <h1 className="banner__title">Movie Name</h1>
-            <div className="banner__buttons">
-                <button className='banner__button'>Play</button>
-                <button className='banner__button'>My List</button>
-            </div>
-            <h1 className="banner__description">
-                {truncate(`This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description`, 150)}
-            </h1>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
-        <div className="banner--fadeBottom" />
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
