a5f003422559c8d8b932a4bf87a4d40e499fc94a
Bryan Sarmiento <SarmientoB2@gmail.com>
2022-11-02 02:02:43 -0700

    Added Sign In Screen

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAEjC_c0p56bI-nxP-uDPycPQEnUU76gnE",
     authDomain: "react-netflix-clone-8c5da.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "1080249022586",
     appId: "1:1080249022586:web:c6b9333c4ccaedc74623c3"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
